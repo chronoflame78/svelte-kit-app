dd3a6b7b847c507084037a34405bcbb88b2abe24
NhatNguyen <nhatnguyen@lgcns.com>
2024-03-13 15:17:09 +0700

    init project

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    let name = 'John';
+</script>
+
+<h1>About</h1>
---
+++ src/routes/product/[id]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import {page} from '$app/stores';
+</script>
+
+
+<h1>Product Page</h1>
+
+<p>Product ID: {$page.params.id}</p>
