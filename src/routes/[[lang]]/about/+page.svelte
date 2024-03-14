492b2624de031514a5e89d4464a4dd6e0dee5c85
NhatNguyen <nhatnguyen@lgcns.com>
2024-03-14 11:25:20 +0700

    rest params, optional params

---
+++ src/routes/[[lang]]/about/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { beforeNavigate, afterNavigate } from '$app/navigation';
+	import { page } from '$app/stores';
+	let name = 'John';
+</script>
+
+<h1>About {name}</h1>
+{JSON.stringify($page.params)}
